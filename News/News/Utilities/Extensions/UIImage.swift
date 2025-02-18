5a64127ddb5897b80674006b4e220ab96f097383
Mert Adem Özseven <developer@mertozseven.com>
2025-02-18 21:43:04 +0300

    Edit: UIImage Extension

--- News/News/Utilities/Extensions/UIImage.swift
+++ News/News/Utilities/Extensions/UIImage.swift
@@ -5,4 +5,17 @@
 //  Created by Mert Ozseven on 18.02.2025.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    /// UIImage extension to resize image
+    /// - Parameters:
+    /// - targetSize: CGSize
+    func resized(to targetSize: CGSize) -> UIImage? {
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+        let newImage = renderer.image { (context) in
+            self.draw(in: CGRect(origin: .zero, size: targetSize))
+        }
+        return newImage
+    }
+}
